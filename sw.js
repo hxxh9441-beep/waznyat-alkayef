16b273115efedef55b1de0a9cc1451ccc18b3550
hxxh9441-beep <hxxh9441@gmail.com>
2026-08-02 11:45:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    التخزين المؤقت لتشغيل PWA بدون إنترنت
    ============================================ */
 
-const CACHE = 'shahicalc-v10';
+const CACHE = 'shahicalc-v15';
 const ASSETS = [
   './',
   './index.html',
